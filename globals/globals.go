86e3a43346175067926a1f5bba78fbab67eeaa8e
KI-Cheol <jja147@naver.com>
2023-12-20 01:34:32 -0500

    comment delete

--- globals/globals.go
+++ globals/globals.go
@@ -13,8 +13,7 @@ var TestClient *kubernetes.Clientset
 func InitClient() {
 	Client = make(map[string]*kubernetes.Clientset)
 	kubeconfigPath := "/root/.kube/config"
-
-	// kubeconfig 파일 로드
+	
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 	if err != nil {
 		// kubeconfig 파일이 없는 경우 InClusterConfig 사용
